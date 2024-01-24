9d88cc75e176fefc7e47d96aa2858a23268b236f
Ilya Novikov <ilyanovickov@mail.ru>
2024-01-24 21:10:46 +0300

    AaaaaaAAAAAAAAAA

---
+++ WpfPaintProj/ExtraControls/DrawingCanvas.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPaintProj.ExtraControls
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl1.xaml
+    /// </summary>
+    public partial class DrawingCanvas : UserControl
+    {
+        private Shape selectedShape = null;
+
+        public DrawingCanvas()
+        {
+            InitializeComponent();
+        }
+
+        public Shape SelectedShape
+        {
+            get => selectedShape;
+            private set
+            {
+                selectedShape = value;
+                if (selectedShape != null)
+                {
+
+                }
+            }
+        }
+
+        public void AddShape(Shape shape)
+        {
+            this.Canvas.Children.Add(shape);
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Ellipse ellipse = new Ellipse();
+
+
+        }
+    }
+}
---
+++ WpfPaintProj/OwnShapes/EllipseV.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+
+namespace WpfPaintProj.OwnShapes
+{
+    internal class EllipseV : Shape
+    {
+        EllipseGeometry ellipse;
+        public static readonly DependencyProperty TextBoxRProperty = DependencyProperty.Register("TextBoxR", typeof(TextBox), typeof(EllipseV), new FrameworkPropertyMetadata(null));
+        public TextBox TextBox
+        {
+            get { return (TextBox)GetValue(TextBoxRProperty); }
+            set { SetValue(TextBoxRProperty, value); }
+        }
+        public EllipseV()
+        {
+            ellipse = new EllipseGeometry();
+
+            this.Stroke = Brushes.Gray;
+            this.StrokeThickness = 3;
+        }
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                TranslateTransform t = new TranslateTransform(ActualWidth / 2, ActualHeight / 2);
+                ellipse.Transform = t;
+                ellipse.RadiusX = this.ActualWidth / 2;
+                ellipse.RadiusY = this.ActualHeight / 2;
+                return ellipse;
+            }
+        }
+    }
+}
