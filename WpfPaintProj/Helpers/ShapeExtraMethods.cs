7a182366d9ea4e04a05626df269a7373a53467dd
Ilya Novikov <ilyanovickov@mail.ru>
2024-01-26 20:46:26 +0300

    Добавлены ещё доп методы

--- WpfPaintProj/Helpers/ShapeExtraMethods.cs
+++ WpfPaintProj/Helpers/ShapeExtraMethods.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Shapes;
 
 namespace WpfPaintProj.Helpers
@@ -15,5 +17,104 @@ namespace WpfPaintProj.Helpers
             Canvas.SetLeft(shape, Canvas.GetLeft(shape) + dx);
             Canvas.SetTop(shape, Canvas.GetTop(shape) + dy);
         }
+
+        public static void SetCanvasPoint(this Shape shape, double x, double y)
+        {
+            Canvas.SetLeft(shape, x);
+            Canvas.SetTop(shape, y);
+        }
+
+        public static Point GetCanvasPoint(this Shape shape)
+        {
+            return new Point(Canvas.GetLeft(shape), Canvas.GetTop(shape));
+        }
+
+        public static IEnumerable<Shape> GetEllipseControlPoints(this Ellipse ellipse)
+        {
+            return GetRectangleControlPoints(new Rect(ellipse.GetCanvasPoint().X, 
+                ellipse.GetCanvasPoint().Y, ellipse.Width, ellipse.Height));
+        }
+
+        public static IEnumerable<Shape> GetRectangleControlPoints(this Rectangle rect)
+        {
+            return GetRectangleControlPoints(new Rect(rect.GetCanvasPoint().X,
+                rect.GetCanvasPoint().Y, rect.Width, rect.Height));
+        }
+
+        private static IEnumerable<Shape> GetRectangleControlPoints(Rect rectangle)
+        {
+            List<Point> points = new List<Point>()
+            {
+                new Point(rectangle.X, rectangle.Y),
+
+                new Point(rectangle.X + rectangle.Width / 2d, rectangle.Y),
+                new Point(rectangle.X + rectangle.Width, rectangle.Y),
+
+                new Point(rectangle.X, rectangle.Y + rectangle.Height / 2d),
+                new Point(rectangle.X, rectangle.Y + rectangle.Height),
+
+                new Point(rectangle.X + rectangle.Width / 2d, rectangle.Y + rectangle.Height),
+                new Point(rectangle.X + rectangle.Width, rectangle.Y + rectangle.Height),
+                new Point(rectangle.X + rectangle.Width, rectangle.Y + rectangle.Height / 2d),
+            };
+
+            List<Shape> shapes = new List<Shape>(1);
+
+            foreach (Point point in points)
+            {
+                Rectangle rect = new Rectangle()
+                {
+                    Width = 10,
+                    Height = 10,
+                    Fill = new SolidColorBrush(Color.FromRgb(0, 255, 0)),
+                    Stroke = new SolidColorBrush(Color.FromRgb(0, 0, 0))
+
+                };
+
+                rect.SetCanvasPoint(point.X - 5, point.Y - 5);
+
+                shapes.Add(rect);
+            }
+
+            return shapes;
+        }
+
+        //public static IEnumerable<Shape> GetRectangleControlPoints(this Rectangle rectangle)
+        //{
+        //    List<Point> points = new List<Point>()
+        //    {
+        //        new Point(rectangle.GetCanvasPoint().X, rectangle.GetCanvasPoint().Y),
+
+        //        new Point(rectangle.GetCanvasPoint().X + rectangle.Width / 2d, rectangle.GetCanvasPoint().Y),
+        //        new Point(rectangle.GetCanvasPoint().X + rectangle.Width, rectangle.GetCanvasPoint().Y),
+
+        //        new Point(rectangle.GetCanvasPoint().X, rectangle.GetCanvasPoint().Y + rectangle.Height / 2d),
+        //        new Point(rectangle.GetCanvasPoint().X, rectangle.GetCanvasPoint().Y + rectangle.Height),
+
+        //        new Point(rectangle.GetCanvasPoint().X + rectangle.Width / 2d, rectangle.GetCanvasPoint().Y + rectangle.Height),
+        //        new Point(rectangle.GetCanvasPoint().X + rectangle.Width, rectangle.GetCanvasPoint().Y + rectangle.Height),
+        //        new Point(rectangle.GetCanvasPoint().X + rectangle.Width, rectangle.GetCanvasPoint().Y + rectangle.Height / 2d),
+        //    };
+
+        //    List<Shape> shapes = new List<Shape>(1);
+
+        //    foreach (Point point in points)
+        //    {
+        //        Rectangle rect = new Rectangle()
+        //        {
+        //            Width = 10,
+        //            Height = 10,
+        //            Fill = new SolidColorBrush(Color.FromRgb(0, 255, 0)),
+        //            Stroke = new SolidColorBrush(Color.FromRgb(0, 0, 0))
+
+        //        };
+
+        //        rect.SetCanvasPoint(point.X - 5, point.Y - 5);
+
+        //        shapes.Add(rect);
+        //    }
+
+        //    return shapes;
+        //}
     }
 }
