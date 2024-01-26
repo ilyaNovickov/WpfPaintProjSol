dd98ae007d87c8bb5d6f707f02b273526a8bca37
Ilya Novikov <ilyanovickov@mail.ru>
2024-01-26 20:08:46 +0300

    Добавлен доп метод

---
+++ WpfPaintProj/Helpers/ShapeExtraMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace WpfPaintProj.Helpers
+{
+    internal static class ShapeExtraMethods
+    {
+        public static void Offset(this Shape shape, double dx, double dy)
+        {
+            Canvas.SetLeft(shape, Canvas.GetLeft(shape) + dx);
+            Canvas.SetTop(shape, Canvas.GetTop(shape) + dy);
+        }
+    }
+}
