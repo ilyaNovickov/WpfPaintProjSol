a9d40e510e01e44270dcc26056543524f0343ca2
Ilya Novikov <ilyanovickov@mail.ru>
2024-01-27 20:56:25 +0300

    Добавлено перечисленрие

---
+++ WpfPaintProj/Helpers/ReseizeDirection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPaintProj.Helpers
+{
+    internal enum ReseizeDirection
+    {
+        Top, Bottom, Left, Right, TopRight, BottomRight, TopLeft, BottomLeft
+    }
+}
